b05c64b16b738986df16c6fe9bd90b2162d32176
Akronyhm <MarcusFlysjo@gmail.com>
2024-06-20 03:52:13 +0200

    Added a raw logger that doesn't look at logging level (in case of the Config being null). Also added new method for getting a Mod-Store directory

--- CSharp/Server/McmUtils.cs
+++ CSharp/Server/McmUtils.cs
@@ -1,10 +1,54 @@
 using Barotrauma;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace MultiplayerCrewManager
 {
     public static class McmUtils
     {
-        public static void Trace(object msg)
+        public static void Raw(object? msg)
+        {
+            try
+            {
+                ModUtils.Logging.PrintMessage($"[RAW] [MCM] - {msg}");
+            }
+            catch (System.Exception e)
+            {
+                ModUtils.Logging.PrintError(e.Message);
+            }
+        }
+
+        public static void Raw(Exception ex, object? msg)
+        {
+            try
+            {
+                ModUtils.Logging.PrintMessage($"[RAW ERROR] [MCM] - {msg}, Exception {ex.Message}");
+                StackTrace stackTrace = new StackTrace(0, true);
+                var maxTrace = Math.Min(3, stackTrace.FrameCount);
+
+                for (int i = 0; i < maxTrace; i++)
+                {
+                    var frame = stackTrace.GetFrame(i);
+                    if (frame == null) continue;
+
+                    // Get the file name and line number
+                    string fileName = frame.GetFileName();
+                    int lineNumber = frame.GetFileLineNumber();
+
+                    // Get the method that called the current method
+                    var callingMethod = frame.GetMethod().Name;
+                    var methodClass = frame.GetMethod().DeclaringType.Name;
+                    ModUtils.Logging.PrintError($" --> \"{fileName}\" | \"{methodClass}.{callingMethod}\", Line {lineNumber}");
+                }
+            }
+            catch (System.Exception e)
+            {
+                ModUtils.Logging.PrintError(e.Message);
+            }
+        }
+
+        public static void Trace(object? msg)
         {
             try
             {
@@ -17,7 +61,7 @@ namespace MultiplayerCrewManager
             }
         }
 
-        public static void Info(object msg)
+        public static void Info(object? msg)
         {
             try
             {
@@ -30,7 +74,7 @@ namespace MultiplayerCrewManager
             }
         }
 
-        public static void Warn(object msg)
+        public static void Warn(object? msg)
         {
             try
             {
@@ -43,7 +87,7 @@ namespace MultiplayerCrewManager
             }
         }
 
-        public static void Error(object msg)
+        public static void Error(object? msg)
         {
             try
             {
@@ -58,13 +102,32 @@ namespace MultiplayerCrewManager
             }
         }
 
-        public static void Error(System.Exception e, object msg)
+        public static void Error(System.Exception e, object? msg)
         {
             try
             {
+                // Create a new stack trace, skipping the first frame to get the caller method
+
                 if (McmMod.GetConfig.LoggingLevel >= McmLoggingLevel.Error) //LoggingLevel=1
                 {
                     ModUtils.Logging.PrintError($"[ERROR] [MCM] - {msg}, Exception {e.Message}");
+                    StackTrace stackTrace = new StackTrace(0, true);
+                    var maxTrace = Math.Min(3, stackTrace.FrameCount);
+
+                    for (int i = 0; i < maxTrace; i++)
+                    {
+                        var frame = stackTrace.GetFrame(i);
+                        if (frame == null) continue;
+
+                        // Get the file name and line number
+                        string fileName = frame.GetFileName();
+                        int lineNumber = frame.GetFileLineNumber();
+
+                        // Get the method that called the current method
+                        var callingMethod = frame.GetMethod().Name;
+                        var methodClass = frame.GetMethod().DeclaringType.Name;
+                        ModUtils.Logging.PrintError($" --> \"{fileName}\" | \"{methodClass}.{callingMethod}\", Line {lineNumber}");
+                    }
                 }
             }
             catch (System.Exception ex)
@@ -72,6 +135,16 @@ namespace MultiplayerCrewManager
                 ModUtils.Logging.PrintError(ex.Message);
             }
         }
-        
+
+        public static string GetModStoreDirectory()
+        {
+            var path = System.IO.Path.Combine("LocalMods/.modstore", "MultiplayerCrewManager");
+            if (false == Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return path;
+        }
+
     }
 }
